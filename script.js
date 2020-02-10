05a8d0af8481fa4fb42e03008b9b83319512f439
Oksana Kushchik <okushchik@gmail.com>
2020-02-10 20:47:52 +0200

    1

---
+++ script.js
@@ -0,0 +1,328 @@
+
+let arr = [
+    {
+        "img": "img/samsung_sm_m105gzbgsek_images_12536158642.jpg",
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/copy_honor_51093fbq_5e312264c4ebb_images_16621311827.jpg',
+        "title": 'Honor 10 Lite 3/64GB Sapphire Blue',
+        "model": 'Honor',
+        "price": '3 999 ₴'
+    },
+    {
+        "img": 'img/copy_xiaomi_redmi_note_8t_524153_5dc3ef6c0a613_images_14964465868.jpg',
+        "title": 'Xiaomi Redmi Note 8T 4/64GB Starscape Blue',
+        "model": 'Xiaomi',
+        "price": '4 999 ₴'
+    },
+    {
+        "img": 'img/huawei_p_smart_pro_6_128_black_images_14991941596.jpg',
+        "title": 'Huawei P Smart Pro 6/128GB Black',
+        "model": 'Huawei',
+        "price": '6 999 ₴'
+    },
+    {
+        "img": 'img/fly_life_compact_dark_blue_images_11474099186.jpg',
+        "title": 'Fly Life Compact Dark Blue',
+        "model": 'Fly',
+        "price": '939 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+    {
+        "img": 'img/samsung_sm_m105gzbgsek_images_12536158642.jpg',
+        "title": 'Samsung Galaxy M10 2/16GB Ocean Blue',
+        "model": 'Samsung',
+        "price": '2 944 ₴'
+    },
+];
+let wallpaper = document.querySelector('.wallpaper');
+let nav;
+let buttonPages;
+let cunt = 1;
+let nuvPages;
+
+
+function navButton(){
+    let nuvButtonList = document.createElement('button');
+    nuvButtonList.className = 'nuvButtonList';
+    nuvButtonList.innerHTML = '<i class="fas fa-list"></i>';
+    nuvButtonList.onclick = listBlock;
+
+    let nuvButtonGrid = document.createElement('button');
+    nuvButtonGrid.className = 'nuvButtonGrid';
+    nuvButtonGrid.innerHTML = '<i class="fas fa-th"></i>';
+    nuvButtonGrid.addEventListener('click',gridBlock);
+
+    nav = document.querySelector('.nav');
+    nav.appendChild(nuvButtonList);
+    nav.appendChild(nuvButtonGrid);
+
+}
+navButton();
+
+function gridBlock() {
+    nav.style.display = 'none';
+
+    let containerGrid = document.createElement('div');
+    containerGrid.className = 'containerGrid';
+    wallpaper.appendChild(containerGrid);
+    let page;
+
+    for (let i = 0; i<arr.length; i++){
+        if (i%8 === 0 || i === 0 ) {
+            page = document.createElement('div');
+            page.className = 'page';
+            containerGrid.appendChild(page);
+        }
+            let itemBlockGrid = document.createElement('div');
+            itemBlockGrid.className = 'itemBlockGrid';
+            containerGrid.appendChild(page);
+
+            let itemGrid = '';
+            itemGrid = itemGrid + `<h2>${arr[i].title} <br></h2>`;
+            itemGrid = itemGrid + `<div class="imgPhoneGrid"><img src="${arr[i].img}" alt="phone"> <br></div> ` ;
+            itemGrid = itemGrid + `<h4>Model : ${arr[i].model}</h4> `;
+            itemGrid = itemGrid + `<h4>Price : ${arr[i].price}</h4>`;
+            itemGrid = itemGrid + '<br>' + '-------------'+ '<br>';
+
+            itemBlockGrid.innerHTML = itemGrid;
+            page.appendChild(itemBlockGrid);
+        }
+
+    nuvPages = document.createElement('div');
+    nuvPages.className = 'nuvPages';
+    containerGrid.appendChild(nuvPages);
+    for (let i = 0; i<arr.length; i++){
+        if (i%8 === 0 || i === 0 ) {
+            buttonPages = document.createElement('button');
+            buttonPages.className = 'buttonPages';
+            buttonPages.innerHTML = `${cunt}`;
+            nuvPages.appendChild(buttonPages);
+            cunt++;
+        }
+    }
+    let arrButton = document.querySelectorAll('.buttonPages');
+    let arrPages = document.querySelectorAll('.page');
+
+    arrButton = Array.from(arrButton);
+    for (let i = 0; i < arr.length; i++) {
+        arrButton[i].addEventListener("click", function(){
+            arrPages.forEach(element => element.style.display = 'none');
+            arrPages[i].style.display = 'flex';
+            arrPages[i].style.flexWrap = 'wrap';
+            arrPages[i].style.marginLeft = '10%';
+        });
+    }
+}
+function listBlock() {
+
+    nav.style.display = 'none';
+
+    let containerList = document.createElement('div');
+    containerList.className = 'containerList';
+    wallpaper.appendChild(containerList);
+    let page;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i%8 === 0 || i === 0 ) {
+            page = document.createElement('div');
+            page.className = 'page';
+            containerList.appendChild(page);
+        }
+        let itemBlockImgList = document.createElement('div');
+        itemBlockImgList.className = 'itemBlockImgList';
+        itemBlockImgList.innerHTML = `<div class="imgPhoneList"><img src="${arr[i].img}" alt="phone"></div> `;
+        containerList.appendChild(itemBlockImgList);
+
+        let itemBlockTitleList = document.createElement('div');
+        itemBlockTitleList.className = 'itemBlockTitleList';
+        let itemList = '';
+        itemList = itemList + `<h2>${arr[i].title} <br></h2>`;
+        itemList = itemList + `<h4>Model : ${arr[i].model}</h4><br> `;
+        itemList = itemList + `<h4>Price : ${arr[i].price}</h4>`;
+        itemList = itemList + '<br>' + '-------------' + '<br>';
+        itemBlockTitleList.innerHTML = itemList;
+
+        let itemBlockList = document.createElement('div');
+        itemBlockList.className = 'itemBlockList';
+
+        itemBlockList.appendChild(itemBlockImgList);
+        itemBlockList.appendChild(itemBlockTitleList);
+
+        page.appendChild(itemBlockList);
+    }
+    nuvPages = document.createElement('div');
+    nuvPages.className = 'nuvPages';
+    containerList.appendChild(nuvPages);
+    for (let i = 0; i<arr.length; i++){
+        if (i%8 === 0 || i === 0 ) {
+            buttonPages = document.createElement('button');
+            buttonPages.className = 'buttonPages';
+            buttonPages.innerHTML = `${cunt}`;
+            nuvPages.appendChild(buttonPages);
+            cunt++;
+
+        }
+    }
+    let arrButton = document.querySelectorAll('.buttonPages');
+    let arrPages = document.querySelectorAll('.page');
+    arrButton = Array.from(arrButton);
+
+
+    for (let i = 0; i < arr.length; i++) {
+        arrButton[i].addEventListener("click", function(){
+        arrPages.forEach(element => element.style.display = 'none');
+        arrPages[i].style.display = 'block';
+        });
+    }
+
+}
+
+
+
